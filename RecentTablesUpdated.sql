e12b1a29e5a2cc1b252ea29b74b5e7621276d937
Jonathan Davidson <jmdavi@umich.edu>
2016-03-25 13:10:48 -0400

    commentary added
    alternative dateparts at
    https://msdn.microsoft.com/en-us/library/ms186819.aspx

--- RecentTablesUpdated.sql
+++ RecentTablesUpdated.sql
@@ -1,6 +1,8 @@
+--returns tables recently updated in database
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 SELECT OBJECT_NAME(OBJECT_ID) AS TableName, last_user_update,*
 FROM sys.dm_db_index_usage_stats s
 WHERE database_id = DB_ID()
 AND last_user_update>DATEADD(MINUTE, -5,SYSDATETIME())
+--alternative dateparts listed at https://msdn.microsoft.com/en-us/library/ms186819.aspx
 ORDER BY s.last_user_update DESC
